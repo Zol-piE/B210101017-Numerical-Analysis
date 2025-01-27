5d516892cbbe8bfe1f3e8f1b6e2c513191403ac9
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2025-01-27 18:54:34 +0600

    Update Forward.cpp

--- Forward.cpp
+++ Forward.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 #define fast ios_base::sync_with_stdio(false); cin.tie(NULL);
 
-void print(float arr[4][4], int n) // specify size of the array
+void print(const vector<vector<float>>& arr, int n) // Using vector
 {
     for(int i = 0; i < n; i++)
     {
@@ -13,69 +13,38 @@ void print(float arr[4][4], int n) // specify size of the array
         cout << endl;
     }
 }
-unsigned int factorial(unsigned int n)
-{
-    if (n == 0)
-        return 1;
-    return n * factorial(n - 1);
-}
-float solve(float y[4][4],float x[4],int n,int h,float val)
-{
-    double  p = (val -x[0]) / h;
 
-    float res = 0;
-    res+= y[0][0];
-    for(int i =1;i<n;i++)
-    {
-        float z = 1;
-        for(int j = 0;j<i;j++)
-        {
-           z *=(p-j);
-        }
-        res += (z * y[0][i]) / (float)factorial(i);
-
-    }
-    return res;
-}
 int main()
 {
     fast
-    int n = 4;
-    float x[n];
-    cout<<"Enter The The Value Of X "<<endl;
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
 
-    for(int i =0;i<4;i++) cin>>x[i];
+    vector<float> x(n);
+    cout << "Enter the values of X: " << endl;
+    for(int i = 0; i < n; i++) cin >> x[i];
 
-    float y[4][4];
 
-    cout<<"Enter The The Value Of Y "<<endl;
+    vector<vector<float>> y(n, vector<float>(n, 0));
+
+    cout << "Enter the values of Y: " << endl;
     for(int i = 0; i < n; i++)
     {
-        for(int j = 0; j < n; j++)
-        {
-            y[i][j] = -1;
-        }
+        cin >> y[i][0];
     }
-    for(int i = 0;i<4;i++)
-    {
-        cin>>y[i][0];
-    }
-//    y[0][0] = .7071;
-//    y[1][0] = .7660;
-//    y[2][0] = .8192;
-//    y[3][0] = .8660;
 
-    for(int i = 1; i < 4; i++)
+
+    for(int i = 1; i < n; i++)
     {
         for(int j = 0; j < (n - i); j++)
         {
             y[j][i] = y[j + 1][i - 1] - y[j][i - 1];
         }
     }
-    cout<<"Print Forward Difference Table"<<endl;
-    print(y, n); // call the print function with n
-    cout<<"Enter The Value Of X for Interpolation " <<endl;
-    float z; cin>>z;
-    cout<<solve(y,x,n,x[1]-x[0],z)<<endl;
+
+    cout << "Print Forward Difference Table:" << endl;
+    print(y, n);
+
     return 0;
 }
